7f80f4403aeebf65a430f17b8a7adb6708fd0732
fenomenogr777 <nc93gate7@hotmail.com>
2023-02-08 18:11:13 +0200

    starting files

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
 'use script';
 
-class Age {
-  curYear = new Date().getFullYear();
-  constructor(birthYear) {
-    this.birthYear = birthYear;
-  }
-  calcBirthYear() {
-    return this.curYear - this.birthYear;
-  }
-}
-
-const nikos = new Age(1993);
-
-console.log(nikos.calcBirthYear());
